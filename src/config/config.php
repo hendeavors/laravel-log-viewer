765e845ad8b8a1e4c726f5d359b74739df117330
Adam Rodriguez <adam_rodriguez@bitbucket.org>
2018-10-02 13:18:01 -0700

    add non-env config support

---
+++ src/config/config.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+     /*
+     |--------------------------------------------------------------------------
+     | Pattern and storage path settings
+     |--------------------------------------------------------------------------
+     |
+     | Support non-env environments.
+     |
+     */
+    'pattern' => '*.log',
+    'storage_path' => 'logs',
+    'server_paging' => false,
+];
